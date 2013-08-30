ce6ef659966878906bda783d9ef6cea4a56fdd0e
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-30 13:36:11 +0200

    get/set customs

--- api_db.py
+++ api_db.py
@@ -5,10 +5,23 @@ PATH = '/gravel/system/api'
 class Ownership(graveldb.Table('ownerships', PATH)):
     default = dict(owner=None)
 
+    @staticmethod
+    def get(kind, ident):
+        assert ':' not in kind
+        return Ownership('%s:%s' % (kind, ident))
+
 def set_owner(kind, ident, owner, overwrite=False):
-    assert ':' not in kind
-    ownership = Ownership('%s:%s' % (kind, ident))
+    ownership = Ownership.get(kind, ident)
     if not overwrite and ownership.exists:
         raise ValueError('object %r already owned' % ownership.name)
     ownership.data.owner = owner
     ownership.save()
+
+def verify_owner(kind, ident, owner):
+    ownership = Ownership.get(kind, ident)
+    if owner != ownership.data.owner:
+        raise PermissionDenied()
+
+class PermissionDenied(Exception):
+    def __init__(self, msg='Permission denied'):
+        Exception.__init__(self, msg)
--- rpcserver.py
+++ rpcserver.py
@@ -4,11 +4,14 @@ import os
 sys.path.append('/gravel/pkg/gravel-common')
 
 import subprocess
+import json
 
 import cmd_util
 import gravelrpc
 import api_db
 import api_config
+
+from api_db import PermissionDenied
 
 secrets_file = 'api_secrets.txt'
 
@@ -34,19 +37,29 @@ class Handler(gravelrpc.RPCHandler):
         if self.client_id not in api_config.ALLOW_USER_CREATION:
             raise PermissionDenied()
 
-        uid = int(master_call(['user', 'add', '%s' % uid], func=subprocess.check_output))
+        uid = int(master_call(['user', 'add', '%d' % uid], func=subprocess.check_output))
         master_call(['storage', 'add', 'u%d' % uid, 'file', 'uid=%d' % uid])
 
         api_db.set_owner('user', uid, self.client_id)
 
         return uid
 
+    def method_get_custom(self, uid, name):
+        api_db.verify_owner('user', uid, self.client_id)
+
+        data = master_call(['user', 'custom', '--get', '--', '%d' % uid, name],
+                           func=subprocess.check_output)
+        return json.loads(data)
+
+    def method_set_custom(self, uid, name, data):
+        api_db.verify_owner('user', uid, self.client_id)
+
+        master_call(['user', 'custom', '--set', '--', '%d' % uid, name],
+                    func=cmd_util.call_with_stdin,
+                    stdin_data=json.dumps(data))
+
 def master_call(args, **kwargs):
     return cmd_util.generic_call(api_config.MASTER_PREFIX + args, **kwargs)
-
-class PermissionDenied(Exception):
-    def __init__(self, msg='Permission denied'):
-        Exception.__init__(self, msg)
 
 if __name__ == '__main__':
     Handler.main(('localhost', 4443), gravelrpc.ThreadingSSLServer)
