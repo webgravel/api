e612a3b9d62e1b01e80e6266e4bfab9fb528d93e
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-30 13:04:34 +0200

    method_create_user

--- api_db.py
+++ api_db.py
@@ -4,3 +4,11 @@ PATH = '/gravel/system/api'
 
 class Ownership(graveldb.Table('ownerships', PATH)):
     default = dict(owner=None)
+
+def set_owner(kind, ident, owner, overwrite=False):
+    assert ':' not in kind
+    ownership = Ownership('%s:%s' % (kind, ident))
+    if not overwrite and ownership.exists:
+        raise ValueError('object %r already owned' % ownership.name)
+    ownership.data.owner = owner
+    ownership.save()
--- rpcserver.py
+++ rpcserver.py
@@ -3,10 +3,12 @@ import os
 
 sys.path.append('/gravel/pkg/gravel-common')
 
-import gravelrpc
-import api_config
+import subprocess
 
+import cmd_util
+import gravelrpc
 import api_db
+import api_config
 
 secrets_file = 'api_secrets.txt'
 
@@ -19,12 +21,32 @@ class Handler(gravelrpc.RPCHandler):
     key = api_config.KEY
 
     def _preprocess_args(self, *args, **kwargs):
-        self.client_id = secrets[kwargs['auth']]
+        try:
+            self.client_id = secrets[kwargs['auth']]
+        except KeyError:
+            raise PermissionDenied('invalid auth key')
         if not self.client_id or kwargs['auth'].startswith('#'):
             raise RuntimeError('malformed secrets file')
+        del kwargs['auth']
+        return args, kwargs
 
-    def method_create_user(self, name):
-        pass
+    def method_create_user(self, uid):
+        if self.client_id not in api_config.ALLOW_USER_CREATION:
+            raise PermissionDenied()
+
+        uid = int(master_call(['user', 'add', '%s' % uid], func=subprocess.check_output))
+        master_call(['storage', 'add', 'u%d' % uid, 'file', 'uid=%d' % uid])
+
+        api_db.set_owner('user', uid, self.client_id)
+
+        return uid
+
+def master_call(args, **kwargs):
+    return cmd_util.generic_call(api_config.MASTER_PREFIX + args, **kwargs)
+
+class PermissionDenied(Exception):
+    def __init__(self, msg='Permission denied'):
+        Exception.__init__(self, msg)
 
 if __name__ == '__main__':
     Handler.main(('localhost', 4443), gravelrpc.ThreadingSSLServer)
